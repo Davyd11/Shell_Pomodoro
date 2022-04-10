341c3ae7a8d60aea2674efc5aad51adb2e57cef4
Javier Gonzalez nicora <javgonza@student.42madrid.com>
2022-04-10 16:40:05 +0200

    Reimplemented the arg_parsing with the argparse library. It provides a -h flag

--- args_parser.py
+++ args_parser.py
@@ -6,31 +6,22 @@
 #    By: javgonza <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/10 14:27:37 by javgonza          #+#    #+#              #
-#    Updated: 2022/04/10 14:57:35 by javgonza         ###   ########.fr        #
+#    Updated: 2022/04/10 16:37:37 by javgonza         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 from timer_printer import TimerPrinter
+import argparse
 
 def parseArgs():
-	
-	if len(sys.argv) > 1:
-		if (sys.argv[1] == "-e"):
-			TimerPrinter.minutes = 49
-			TimerPrinter.breakMinutes = 10
-		elif (sys.argv[1] == "-c"):
-		      return parseCustomTime()
-		else:
-			print ( TimerPrinter.RedColor + "\nYour argument is not well written\nFor a extended pomodoro use the argument [ -e ]\n" + TimerPrinter.ResetColor)
-			return 1
-	return 0
-	
-def parseCustomTime():
-	if len(sys.argv) != 4:
-		print ( TimerPrinter.RedColor + "\nYour argument is not well written\nFor custom pomodoro use:\n\tpomodoro -c <minutes> <seconds>\nExample:\n\tpomodoro -c 35 12" + TimerPrinter.ResetColor)
-		return 1
-	else:
-		TimerPrinter.minutes = int(sys.argv[2])
-		TimerPrinter.seconds = int(sys.argv[3])
-		return 0
+	parser = argparse.ArgumentParser(prog='Shell Pomodoro', description='A pomodoro timer for your shell')
+	parser.add_argument('-c', '--custom_time', nargs=2,  type=int, metavar=('minutes', 'seconds'), help='Add a custom time to your pomodoro')
+	parser.add_argument('-e', '--extended', help='Extended time. 50 minutes and 10 break minutes. Incompatible with --custom_time.', action='store_true')
+	timer_args = parser.parse_args()
+	if (timer_args.custom_time != None):
+		TimerPrinter.minutes = timer_args.custom_time[0]
+		TimerPrinter.seconds = timer_args.custom_time[1]
+	elif (timer_args.extended == True):
+		TimerPrinter.minutes = 49
+		TimerPrinter.breakMinutes = 49
